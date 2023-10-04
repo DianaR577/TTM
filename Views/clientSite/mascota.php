8ada64c9449dfc068f6845af248fe580c07e2702
DianaR577 <139731994+DianaR577@users.noreply.github.com>
2023-10-04 08:17:02 -0500

    Add files via upload

---
+++ Views/clientSite/mascota.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+
+<!--
+ // WEBSITE: https://themefisher.com
+ // TWITTER: https://twitter.com/themefisher
+ // FACEBOOK: https://www.facebook.com/themefisher
+ // GITHUB: https://github.com/themefisher/
+-->
+
+<html lang="en">
+
+<head>
+
+	<!-- ** Basic Page Needs ** -->
+	<meta charset="utf-8">
+	<title>Mascota - TTM</title>
+
+	<!-- ** Mobile Specific Metas ** -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Agency HTML Template">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+	<meta name="author" content="Themefisher">
+	<meta name="generator" content="Themefisher Classified Marketplace Template v1.0">
+
+	<!-- favicon -->
+	<link href="../images/favicon.png" rel="shortcut icon">
+
+	<!-- 
+  Essential stylesheets
+  =====================================-->
+  	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+	<link href="../plugins/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<link href="../plugins/bootstrap/bootstrap-slider.css" rel="stylesheet">
+	<link href="../plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="../plugins/slick/slick.css" rel="stylesheet">
+	<link href="../plugins/slick/slick-theme.css" rel="stylesheet">
+	<link href="../plugins/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+
+	<link href="../css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="body-wrapper">
+
+<!-- header insertado con PHP -->
+	<?php
+		include("nav-clientSite.php");
+	?>
+	
+	<!--=================================
+=            Single Blog            =
+==================================-->
+	<section class="blog single-blog section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8">
+					<article class="single-post">
+						<p class="text-right mb-0"><a href="mascotas.php"><i class=" fa fa-chevron-left mr-1"></i>Volver</a></p>
+						<h2 >Simba</h2>
+						<p>Fundación cat in home Jul 19, 2023 </p>
+						<img src="../../Uploads/mascotas/mascotaEsp.jpg" alt="article-01">
+					<h3>Personalidad:</h3>
+					<p>Simba es un gato muy sociable y se lleva bien con personas de todas las edades. Le encanta recibir atención y caricias, y disfruta pasar tiempo cerca de sus seres queridos. Es un compañero leal y estará encantado de acurrucarse contigo en el sofá o en la cama. También le gusta jugar y perseguir juguetes, lo que le proporciona ejercicio y diversión.</p>
+
+					<h3>Historial de salud:</h3>
+					<p>Simba ha sido sometido a un examen veterinario completo y está en buen estado de salud. Ha sido desparasitado, esterilizado y está al día con todas sus vacunas. También está entrenado para usar la caja de arena y no presenta problemas de comportamiento.</p>
+
+					<h3>Requisitos de adopción:</h3>
+					<p>Simba busca un hogar donde reciba mucho amor y atención. Necesita un espacio seguro y enriquecedor para explorar y jugar. Preferiblemente, se dará prioridad a las familias o personas que ya tengan experiencia previa con gatos y estén comprometidas a cuidar de él durante toda su vida.</p>
+
+					<h4>Si estás interesado en adoptar a Simba, por favor, ponte en contacto con nosotros para obtener más información y programar una visita. ¡Simba está ansioso por encontrar a su familia perfecta y comenzar una nueva aventura juntos!</h4>
+					
+					<!-- <a class="btn btn-login mt-5" href="for_adop.php">Formulario de adopción</a> -->
+
+					</article>
+
+				</div>
+				<div class="col-lg-4">
+					<div class="sidebar">
+					<!-- Category Widget -->
+					<div class="widget category">
+						<!-- Widget Header -->
+						<h5 class="widget-header">Fundaciones</h5>
+						<ul class="category-list">
+							<li><a href="category.html">Fundaciones de gatos <span class="float-right">(2)</span></a></li>
+							<li><a href="category.html">Fundaciones de perros  <span class="float-right">(5)</span></a></li>
+							<li><a href="category.html">Fundaciones de aves <span class="float-right">(7)</span></a></li>
+						</ul>
+					</div>
+					<!-- Archive Widget -->
+					<div class="widget category">
+						<!-- Widget Header -->
+						<h5 class="widget-header">Información específica</h5>
+						<ul class="category-list">
+							<li><strong>Nombre:</strong> Simba</li>
+							<li><strong>Sexo:</strong> Masculino</li>
+							<li><strong>Edad:</strong> 5 meses</li>
+							<li><strong>Raza:</strong> Atigrado</li>
+							<li><strong>Color:</strong> Gris</li>
+						</ul>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!--============================
+=            Footer            =
+=============================-->
+
+	<?php
+	include("footer-include.php")
+	?>
+
+	<!-- 
+Essential Scripts
+=====================================-->
+	<script src="../plugins/jquery/jquery.min.js"></script>
+	<script src="../plugins/bootstrap/popper.min.js"></script>
+	<script src="../plugins/bootstrap/bootstrap.min.js"></script>
+	<script src="../plugins/bootstrap/bootstrap-slider.js"></script>
+	<script src="../plugins/tether/js/tether.min.js"></script>
+	<script src="../plugins/raty/jquery.raty-fa.js"></script>
+	<script src="../plugins/slick/slick.min.js"></script>
+	<script src="../plugins/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+	<!-- google map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU" defer></script>
+	<script src="../plugins/google-map/map.js" defer></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+	<script src="../js/script.js"></script>
+
+</body>
+
+</html>
